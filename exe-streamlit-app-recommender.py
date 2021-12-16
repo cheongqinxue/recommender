25b0b2a12637f7ff80c003557a876419c838a6f8
cheongqinxue <58881356+cheongqinxue@users.noreply.github.com>
2021-12-16 16:47:48 +0800

    Add files via upload

---
+++ exe-streamlit-app-recommender.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import faiss
+from faiss import METRIC_INNER_PRODUCT
+import pandas as pd
+import joblib
+import numpy as np
+import torch
+from bipartite_models import TransRBipartiteModel
+import requests
+
+class Args:
+    datapath = 'streamlit'
+    modelpath = 'models/transrBipartite-marginloss0_5-800epoch-5neg'
+
+def load(path):
+    df = joblib.load(path+'/df.joblib')
+    df.published_date = pd.to_datetime(df.published_date, errors='coerce', unit='s')
+    emb = np.load(path+'/embeds.np.npy')
+    return df, emb
+
+def criteria(idx, col, fn):
+    try:
+        x = ds[idx]
+        return fn(x[col])
+    except:
+        return False
+
+def search(domain, rep_vectors, faiss_index, df, head2ix, embeddings, model, display_top_n=20, 
+    search_n_per_signpost=5000, language='any', debug=False, favor='na'):
+    favor = favor.split(',')
+    if all([sn.isnumeric() for sn in favor]):
+        favor = [int(sn) for sn in favor]
+        _, scores, indices = faiss_index.range_search(embeddings[favor,:], 0.55)
+    else:
+        scores, indices = faiss_index.search(torch.vstack(rep_vectors['rep_vectors'][domain]).numpy(), 
+            search_n_per_signpost)
+    indices = list(set(indices.reshape(-1).tolist()))
+
+    with torch.no_grad():
+        h = head2ix[domain]
+        te = torch.tensor(embeddings[indices], device='cpu')
+        scores = model.scoring_function(
+                h_idx=torch.tensor([h], device = 'cpu'),
+                r_idx=torch.tensor([0], device = 'cpu'),
+                t_idx=None,
+                new_tails=te)
+        scores = torch.tanh(scores+2.5)
+        topn = torch.argsort(scores, descending=True)[:max(300, int(search_n_per_signpost/4))].tolist()
+
+    indices_ = np.asarray(indices)[topn].tolist()
+    scores_ = scores[topn].numpy().tolist()
+    resultdf = df.iloc[indices_].drop(columns=['media_item_id','published_date'])
+    resultdf['score'] = scores_
+    resultdf = resultdf.drop_duplicates(subset='title')
+    if language != 'any':
+        resultdf = resultdf[resultdf.language==language]
+    resultdf = resultdf.drop(columns=['language'])
+    try:
+        return resultdf.head(display_top_n)
+    except Exception as e:
+        print('topn ', topn[:10])
+        print('indices ', indices[:10])
+        if debug:
+            raise(e)
+        else:
+            print(e)
+            return topn, indices
+    return
+
+def render(container, **kwargs):
+    resultdf = search(**kwargs)
+    resultdf.style.set_properties(subset='title', **{'width':'200px'})
+    resultdf.style.set_properties(subset='content', **{'width':'500px'})
+
+    if resultdf is None:
+        raise "search failed"
+    else:
+        container.table(resultdf.style) #, height=900, width=1200)
+
+def main(args):
+    pd.set_option('display.max_colwidth', None)
+    df, embeddings = load(args.datapath)
+    languages = ['any','en','es','pt'] + sorted(list(df.dropna(subset=['language']).language.unique()))
+
+    string_factory = 'IVF256,Flat'
+    print('Building index...', end='')
+    index = faiss.index_factory(384, string_factory, METRIC_INNER_PRODUCT)
+    index.train(embeddings)
+    index.add(embeddings)
+    index.nprobe = 8
+    print('Done! ')
+    print('Loading recommender model...',end='')
+    model, head2ix = TransRBipartiteModel.load_pretrained(args.modelpath)
+    rep_vectors = torch.load(args.modelpath+'/rep_vectors.pt')
+    print('Done!')
+
+    c1 = st.container()
+    c2 = st.container()
+
+    du = st.sidebar.selectbox(label = 'Select your domain unit', options=sorted(list(head2ix.keys())), 
+        index=0, key=None, help=None)
+
+    lang = st.sidebar.selectbox(label = 'Select your preferred language', options=languages)
+
+    sn = st.sidebar.text_input(label = 'Enter the serial numbers of preferred news', value='E.g. 1254,5561')
+
+    c1.title('Recommender Demo')
+
+    render(container = c2, **{'domain':du, 'rep_vectors':rep_vectors, 'faiss_index':index, 'df':df, 
+        'head2ix':head2ix, 'embeddings':embeddings, 'model':model, 'language':lang, 'favor':sn})
+
+
+if __name__ == '__main__':
+    args = Args()
+    main(args)
